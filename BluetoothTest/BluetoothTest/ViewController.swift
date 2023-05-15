b1cca93e7f630d0f3ae92b8e18f5c9ba8df1977a
wnsxor1993 <wnsxor1993@naver.com>
2023-05-15 22:34:45 +0900

    feat: 초기 필요 컴포넌트 구현

--- BluetoothTest/BluetoothTest/ViewController.swift
+++ BluetoothTest/BluetoothTest/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bluetoothIDLabel: UILabel!
+    @IBOutlet weak var otherIDStackView: UIStackView!
+    @IBOutlet weak var actionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.configureAttributes()
     }
-
-
 }
 
+private extension ViewController {
+    
+    func configureAttributes() {
+        self.actionButton.layer.cornerRadius = 15
+        self.bluetoothIDLabel.sizeToFit()
+    }
+}
