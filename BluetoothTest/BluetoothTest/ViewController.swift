c762d8a057fd06aa7bc50385d467da4ef3824c08
wnsxor1993 <wnsxor1993@naver.com>
2023-05-15 23:33:03 +0900

    feat: peripheralService 생성 구문 구현

--- BluetoothTest/BluetoothTest/ViewController.swift
+++ BluetoothTest/BluetoothTest/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
 
@@ -13,10 +14,73 @@ class ViewController: UIViewController {
     @IBOutlet weak var otherIDStackView: UIStackView!
     @IBOutlet weak var actionButton: UIButton!
     
+    // MARK: Peripheral
+    private var peripheralManager: CBPeripheralManager?
+    private var service: CBMutableService?
+    private var characteristic: CBMutableCharacteristic?
+    
+    private var serviceUUID: CBUUID?
+    private var characteristicUUID: CBUUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.configureAttributes()
+        self.configureIntializing()
+    }
+}
+
+extension ViewController: CBPeripheralManagerDelegate {
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        switch peripheral.state {
+        case .unknown:
+            print("State is unknown")
+            
+        case .resetting:
+            print("State is resetting")
+            
+        case .unsupported:
+            print("State is unsupported")
+            
+        case .unauthorized:
+            print("State is unauthorized")
+            
+        case .poweredOff:
+            print("State is poweredOff")
+            
+        case .poweredOn:
+            print("State is poweredOn")
+            
+            self.addPeripheralService()
+            
+        @unknown default:
+            print("State is ---")
+        }
+    }
+    
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        if let error {
+            print("Get Error: \(error.localizedDescription)")
+        }
+        
+        print("Start Advertising")
+    }
+}
+
+private extension ViewController {
+    
+    func addPeripheralService() {
+        guard let serviceUUID, let characteristicUUID else { return }
+        
+        self.service = .init(type: serviceUUID, primary: true)
+        // 일단은 읽기만 가능한 형태로?
+        self.characteristic = .init(type: characteristicUUID, properties: [.read], value: nil, permissions: [.readable])
+        
+        guard let service, let characteristic else { return }
+        
+        service.characteristics = [characteristic]
+        self.peripheralManager?.add(service)
     }
 }
 
@@ -25,5 +89,11 @@ private extension ViewController {
     func configureAttributes() {
         self.actionButton.layer.cornerRadius = 15
         self.bluetoothIDLabel.sizeToFit()
+    }
+    
+    func configureIntializing() {
+        self.peripheralManager = .init(delegate: self, queue: .global(qos: .background))
+        self.serviceUUID = .init(nsuuid: .init())
+        self.characteristicUUID = .init(nsuuid: .init())
     }
 }
