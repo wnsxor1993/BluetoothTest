63d3bbe6a399aaf8a0709b9f52dab518cabd3024
wnsxor1993 <wnsxor1993@naver.com>
2023-05-15 22:23:30 +0900

    chore: 초기 프로젝트 생성

---
+++ BluetoothTest/BluetoothTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothTest
+//
+//  Created by juntaek.oh on 2023/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
